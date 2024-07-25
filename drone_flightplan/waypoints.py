729cfca8bb6f00ca566ebfbeb0bb33ea052d48dc
Niraj Adhikari <nrjadkry@gmail.com>
2024-07-25 10:10:50 +0545

    feat: program to generate waypoints within a polygon created

--- drone_flightplan/waypoints.py
+++ drone_flightplan/waypoints.py
@@ -1,4 +1,12 @@
+import logging
+import argparse
+import pyproj
+import geojson
 from shapely.geometry import Polygon
+
+
+# Instantiate logger
+log = logging.getLogger(__name__)
 
 
 def calculate_parameters(
@@ -384,3 +392,152 @@ def generate_waypoints_within_polygon(
     )
 
     return waypoints
+
+
+def create_waypoint(
+    project_area, agl, forward_overlap, side_overlap, generate_each_points, generate_3d
+):
+    """
+    Create waypoints for a given project area based on specified parameters.
+
+    Parameters:
+        project_area (dict): GeoJSON dictionary representing the project area.
+        agl (float): Altitude above ground level.
+        forward_overlap (float): Forward overlap percentage for the waypoints.
+        side_overlap (float): Side overlap percentage for the waypoints.
+        generate_each_points (bool): Flag to determine if each point should be generated.
+        generate_3d (bool): Flag to determine if 3D waypoints should be generated.
+
+    Returns:
+        list: List of waypoints generated within the project area.
+
+    Example Response:
+        [
+            {
+            "coordinates": [
+                85.3199287413415,
+                27.71246834707544
+            ],
+            "angle": "-90",
+            "take_photo": false,
+            "gimbal_angle": "-90"
+            },
+            ...
+        ]
+
+    """
+
+    parameters = calculate_parameters(agl, forward_overlap, side_overlap)
+
+    side_spacing = parameters["side_spacing"]
+    forward_spacing = parameters["forward_spacing"]
+
+    # transform to 3857
+    polygon = Polygon(project_area["geometry"]["coordinates"][0])
+
+    # Define the coordinate systems
+    wgs84 = pyproj.CRS("EPSG:4326")
+    web_mercator = pyproj.CRS("EPSG:3857")
+
+    # Define a transformer to convert from WGS 84 to Web Mercator
+    transformer_to_3857 = pyproj.Transformer.from_crs(
+        wgs84, web_mercator, always_xy=True
+    )
+    transformer_to_4326 = pyproj.Transformer.from_crs(
+        web_mercator, wgs84, always_xy=True
+    )
+
+    polygon_3857 = Polygon(
+        [
+            transformer_to_3857.transform(lon, lat)
+            for lon, lat in polygon.exterior.coords
+        ]
+    )
+
+    distance_between_lines_x = forward_spacing
+    distance_between_lines_y = side_spacing
+
+    waypoints = generate_waypoints_within_polygon(
+        polygon_3857,
+        distance_between_lines_x,
+        distance_between_lines_y,
+        generate_each_points,
+        generate_3d,
+    )
+
+    print("count of waypoints = ", len(waypoints))
+
+    waypoints_4326 = [
+        {
+            "coordinates": transformer_to_4326.transform(
+                wp["coordinates"][0], wp["coordinates"][1]
+            ),
+            "angle": wp["angle"],
+            "take_photo": wp["take_photo"],
+            "gimbal_angle": wp["gimbal_angle"],
+        }
+        for wp in waypoints
+    ]
+    return waypoints_4326
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Generate waypoints for drone missions."
+    )
+    parser.add_argument(
+        "--project_geojson_polygon",
+        required=True,
+        type=str,
+        help="The GeoJSON polygon representing the area of interest.",
+    )
+    parser.add_argument(
+        "--altitude_above_ground_level",
+        required=True,
+        type=float,
+        help="The flight altitude in meters.",
+    )
+    parser.add_argument(
+        "--forward_overlap",
+        type=float,
+        default=70.0,
+        help="The forward overlap in percentage.",
+    )
+    parser.add_argument(
+        "--side_overlap",
+        type=float,
+        default=70.0,
+        help="The side overlap in percentage.",
+    )
+    parser.add_argument(
+        "--generate_each_points",
+        action="store_true",
+        help="Do you want waypoints or waylines.",
+    )
+    parser.add_argument(
+        "--generate_3d", action="store_true", help="Do you want to generate 3D Imagery"
+    )
+
+    args = parser.parse_args()
+
+    with open(args.project_geojson_polygon, "r") as f:
+        boundary = geojson.load(f)
+
+    features = boundary["features"][0]
+
+    coordinates = create_waypoint(
+        features,
+        args.altitude_above_ground_level,
+        args.forward_overlap,
+        args.side_overlap,
+        args.generate_each_points,
+        args.generate_3d,
+    )
+
+    return coordinates
+
+
+if __name__ == "__main__":
+    main()
+
+# python3 waypoints.py  --forward_overlap 80 --side_overlap 75 --project_geojson_polygon '/home/niraj/0_1_sq_km_above_bpbd_office.geojson' --altitude_above_ground_level 100
